94d5c31d70429ec07b43d6d3ad36d3a5d82ab3f7
Vytautas Urbikas <biiccs@gmail.com>
2015-09-21 22:19:55 +0300

    shell install update

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Move phantomjs file to /usr/local/bin
 
 ```
+git copy https://github.com/biiccs/phantomjs-v2-ubuntu14.04.git
+cd phantomjs-v2-ubuntu14.04
 sudo chmod 777 install.sh
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -8,13 +8,7 @@ fi
 if [ `which phantomjs` ]; then
 	echo "You have already installed phantomjs!"
 else
-  cd /tmp
-  rm -rf /tmp/phantomjs-v2-ubuntu14.04
-  git clone https://github.com/biiccs/phantomjs-v2-ubuntu14.04.git
-  cd phantomjs-v2-ubuntu14.04/
   cp phantomjs /usr/local/bin/
   phantomjs download.js http://google.lt google.jpg
-  cd /tmp
-  rm -rf /tmp/phantomjs-v2-ubuntu14.04
 	echo "You have successfully installed phantomjs!"
 fi
